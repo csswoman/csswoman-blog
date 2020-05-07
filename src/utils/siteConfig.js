7fc370d1d15abaf9d8c52f1cb9553d91af50dc2d
Karla Agraz <karla.agraz@gmail.com>
2020-05-06 22:16:21 -0500

    primera version del blog

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-    siteUrl: `https://gatsby.ghost.org`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://csswoman.herokuapp.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `CSS Woman - Karla Agraz`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Blog & Portafolio de Karla Agraz`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
